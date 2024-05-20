c8f028df7d5a0a7f06ec27cc3e6ee8c1f68b2d56
fallienop <130351678+fallienop@users.noreply.github.com>
2024-05-20 17:42:24 +0400

    comx

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,46 +1,4 @@
-// import useAuth from "../services/authService";
 
-// export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-// export const LOGIN_ERROR = "LOGIN_ERROR";
-// export const LOGOUT = "LOGOUT";
-
-
-// const authService = useAuth(); 
-
-// const loginSuccess = user => {
-//     return {
-//         type: LOGIN_SUCCESS,
-//         user
-//     };
-// };
-
-// const loginError = error => {
-//     return {
-//         type: LOGIN_ERROR,
-//         error
-//     };
-// };
-
-// export const login = (username, password) => {
-//     return dispatch => {
-//         authService.login(username, password)
-//             .then(data => {
-//                 data.message
-//                     ? dispatch(loginError(data.message))
-//                     : (dispatch(loginSuccess(data)))
-//             })
-//             .catch(err => dispatch(loginError(err)));
-//     }
-// }
-
-// export const logout = () => {
-//     authService.logout();
-//     return {
-//         type: LOGOUT
-//     };
-// }
-
-// AuthActionsComponent.jsx
 
 import useAuth from "../services/authService";
 
@@ -75,6 +33,18 @@ const AuthActionsComponent = () => {
         }
     }
 
+    const signup=(nameSurname,userName, email,password,phoneNumber)=>{
+        return dispatch => {
+            authService.signUp(nameSurname,userName, email,password,phoneNumber)
+                .then(data => {
+                    data.message
+                        ? dispatch(loginError(data.message))
+                        : (dispatch(loginSuccess(data)))
+                })
+                .catch(err => dispatch(loginError(err)));
+        }
+    }
+
     const logout = () => {
         authService.logout();
         return {
@@ -82,7 +52,7 @@ const AuthActionsComponent = () => {
         };
     }
 
-    return { login, logout };
+    return { login, logout, signup };
 }
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_ERROR = "LOGIN_ERROR";
